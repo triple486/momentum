b6e8c9a7248e1ccd05956acfd011ae83a4335dae
triple486 <triple486@gmail.com>
2022-06-17 15:32:26 +0900

    objects

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-const toBuy = ["potato", "tomato", "pizza"];
-console.log(toBuy[2]);
+const player = {
+  name: "nico",
+  points: 10,
+  fat: true,
+};
+console.log(player);
+player.points = player.points + 15;
+console.log(player);
