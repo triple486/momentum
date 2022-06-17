7c247577311f7c2882cc013a0e33b6ecdf308292
triple486 <triple486@gmail.com>
2022-06-17 16:07:07 +0900

    functions part two

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-function plus(firstNumber, secondNumber) {
-  console.log(firstNumber + secondNumber);
-}
-function divide(a, b) {
-  console.log(a / b);
-}
-plus(60, 8);
-divide(98, 20);
+const player = {
+  name: "nico",
+  sayHello: function (otherPersonsName) {
+    console.log("hello " + otherPersonsName + " nice to meet you!");
+  },
+};
+
+player.sayHello("lynn");
+player.sayHello("nico");
