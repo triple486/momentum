b6e2aff2d98ae0d0138b66aa88f47ead14e22fd5
triple486 <triple486@gmail.com>
2022-06-18 16:08:22 +0900

    returns - calculator

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const player = {
-  name: "nico",
-  sayHello: function (otherPersonsName) {
-    console.log("hello " + otherPersonsName + " nice to meet you!");
+const calculator = {
+  plus: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  times: function (a, b) {
+    console.log(a * b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  power: function (a, b) {
+    console.log(a ** b);
   },
 };
 
-player.sayHello("lynn");
-player.sayHello("nico");
+calculator.plus(2, 3);
+calculator.times(2, 3);
+calculator.minus(2, 3);
+calculator.divide(2, 3);
+calculator.power(2, 3);
