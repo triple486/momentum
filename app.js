5030c5368fa11fe9ca9d2073aa08239135a53d9b
triple486 <triple486@gmail.com>
2022-06-19 14:55:49 +0900

    conditionals part one

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-const calculator = {
-  plus: function (a, b) {
-    console.log(a + b);
-  },
-  minus: function (a, b) {
-    console.log(a - b);
-  },
-  times: function (a, b) {
-    console.log(a * b);
-  },
-  divide: function (a, b) {
-    console.log(a / b);
-  },
-  power: function (a, b) {
-    console.log(a ** b);
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-calculator.plus(2, 3);
-calculator.times(2, 3);
-calculator.minus(2, 3);
-calculator.divide(2, 3);
-calculator.power(2, 3);
+console.log(age);
